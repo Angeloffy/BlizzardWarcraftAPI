e481dcda30a2f798092f3d06b08296819eb2e699
Angeloffy <angeloffy.work@gmail.com>
2023-06-12 12:55:11 +0300

    Added region dicts

---
+++ BlizzardWarcraftAPI/data.py
@@ -0,0 +1,16 @@
+region_data = ["us", "eu", "kr", "tw"]
+
+region_urls_data = {
+    "us": "	https://us.api.blizzard.com/",
+    "eu": "https://eu.api.blizzard.com/",
+    "kr": "https://kr.api.blizzard.com/",
+    "tw": "https://tw.api.blizzard.com/"
+}
+
+locales_data = ["en_US", "es_MX", "pt_BR", "en_GB", "es_ES", "fr_FR", "ru_RU", "de_DE", "pt_PT", "it_IT", "ko_KR",
+                "zh_TW"]
+
+locales_region_data = dict(us=["en_US", "es_MX", "pt_BR"],
+                           eu=["en_GB", "es_ES", "fr_FR", "ru_RU", "de_DE", "pt_PT", "it_IT"],
+                           kr=["ko_KR"],
+                           tw=["zh_TW"])
