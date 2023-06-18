d12a8d3cda4f6eebdc086a343580934ee8a3b4d8
Angeloffy <angeloffy.work@gmail.com>
2023-06-18 14:06:12 +0300

    (Auction House API) Added module

---
+++ BlizzardWarcraftAPI/modules/AuctionHouseAPI.py
@@ -0,0 +1,34 @@
+from .BlizzardWarcraftAPI_base import BlizzardWarcraftAPI
+
+
+class AuctionHouseAPI(BlizzardWarcraftAPI):
+
+    def get_Auctions(self, connectedRealmId) -> dict:
+        """
+        See the Connected Realm API for information about retrieving a list of connected realm IDs.
+        Auction house data updates at a set interval. The value was initially set at 1 hour; however, it might change
+        over time without notice.
+        Depending on the number of active auctions on the specified connected realm, the response from this endpoint may
+        be rather large, sometimes exceeding 10 MB.
+
+        :param connectedRealmId: The ID of the connected realm.
+        :return: Returns all active auctions for a connected realm.
+        :rtype: dict
+        """
+        url = f"https://{self.region}.api.blizzard.com/data/wow/connected-realm/{connectedRealmId}/auctions"
+        self.param["namespace"] = self.namespace_dynamic
+        return self.response(url, self.param)
+
+    def get_Commodities(self) -> dict:
+        """
+        Auction house data updates at a set interval. The value was initially set at 1 hour; however,
+        it might change over time without notice.
+        Depending on the number of active auctions on the specified connected realm,
+        the response from this endpoint may be rather large, sometimes exceeding 10 MB.
+
+        :return: Returns all active auctions for commodity items for the entire game region.
+        :rtype: dict
+        """
+        url = f"https://{self.region}.api.blizzard.com/data/wow/auctions/commodities"
+        self.param["namespace"] = self.namespace_dynamic
+        return self.response(url, self.param)
---
+++ tests/AuctionHouseAPI_test.py
@@ -0,0 +1,13 @@
+from pprint import pprint
+
+from test_base import BlizzardWarcraftAPI_test
+from unittest import TestCase
+
+
+class Test_AuctionHouseAPI(BlizzardWarcraftAPI_test, TestCase):
+
+    def test_Auctions(self):
+        self.BlizzardWarcraftAPI.get_Auctions(1305)
+
+    def test_Commodities(self):
+        self.BlizzardWarcraftAPI.get_Commodities()
