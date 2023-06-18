19c556e777a5b57b3acd119d698f55102d376540
Angeloffy <angeloffy.work@gmail.com>
2023-06-18 14:07:07 +0300

    (Character Appearance API) Added module

---
+++ BlizzardWarcraftAPI/modules/CharacterAppearanceAPI.py
@@ -0,0 +1,16 @@
+from .BlizzardWarcraftAPI_base import BlizzardWarcraftAPI
+
+
+class CharacterAppearanceAPI(BlizzardWarcraftAPI):
+
+    def get_CharacterAppearanceSummary(self, realmSlug: str, characterName: str) -> dict:
+        """
+        :param realmSlug: The slug of the realm.
+        :param characterName: The lowercase name of the character.
+
+        :return: Returns a summary of a character's appearance settings.
+        :rtype: dict
+        """
+        url = f"https://{self.region}.api.blizzard.com/profile/wow/character/{realmSlug}/{characterName}/appearance"
+        self.param["namespace"] = self.namespace_profile
+        return self.response(url, self.param)
---
+++ tests/CharacterAppearanceAPI_test.py
@@ -0,0 +1,10 @@
+from pprint import pprint
+
+from test_base import BlizzardWarcraftAPI_test
+from unittest import TestCase
+
+
+class Test_CharacterAppearanceAPI(BlizzardWarcraftAPI_test, TestCase):
+
+    def test_CharacterAppearanceSummary(self):
+        self.BlizzardWarcraftAPI.get_CharacterAppearanceSummary("deepholm", "дъикиймвп")
