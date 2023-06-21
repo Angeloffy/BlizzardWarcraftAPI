2914b5c0284f83527dc4a315a2a96c34abace1d4
Angeloffy <angeloffy.work@gmail.com>
2023-06-21 13:34:09 +0300

    feat(Character Encounters, Character Equipment, Character Hunter Pets)

--- BlizzardWarcraftAPI/modules/__init__.py
+++ BlizzardWarcraftAPI/modules/__init__.py
@@ -5,5 +5,8 @@ from .gameData.ConnectedRealmAPI import ConnectedRealmAPI
 from .profile.CharacterAchievementsAPI import CharacterAchievementsAPI
 from .profile.CharacterAppearanceAPI import CharacterAppearanceAPI
 from .profile.CharacterCollectionsAPI import CharacterCollectionsAPI
+from .profile.CharacterEncountersAPI import CharacterEncountersAPI
+from .profile.CharacterEquipmentAPI import CharacterEquipmentAPI
+from .profile.CharacterHunterPetsAPI import CharacterHunterPetsAPI
 
 from BlizzardWarcraftAPI.BlizzardAuthToken import BlizzardAuthToken
